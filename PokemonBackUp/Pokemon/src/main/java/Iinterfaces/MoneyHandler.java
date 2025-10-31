da966c79d50b8ff7dd2ae1b11a809a8e57d334c5
SilberHans <jebarajasm@gmail.com>
2025-10-31 11:54:08 -0500

    Add files via upload

---
+++ PokemonBackUp/Pokemon/src/main/java/Iinterfaces/MoneyHandler.java
@@ -0,0 +1,5 @@
+package Iinterfaces;
+
+public interface MoneyHandler {
+    
+}
---
+++ PokemonBackUp/Pokemon/src/main/java/Persons/Trader.java
@@ -0,0 +1,14 @@
+package Persons;
+
+import OtherClasses.Item;
+import java.util.ArrayList;
+
+public class Trader extends Person{
+    private ArrayList<Item> mInventory;
+    private double mPriceMultiplier;
+
+    @Override
+    public void genericDialogue() {
+    
+    }
+}
