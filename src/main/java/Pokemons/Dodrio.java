339b7721e8a3ae5b3dce470f613bdc885518473e
servilag <servilagmegaloco@gmail.com>
2025-11-07 10:47:36 -0500

    añadi el tipo y pokemon que me falto

---
+++ src/main/java/Pokemons/Dodrio.java
@@ -0,0 +1,14 @@
+package Pokemons;
+import Iinterfaces.Flying;
+import Iinterfaces.Normal;
+
+public class Dodrio extends Pokemon implements Flying,Normal{
+    @Override
+    public void growl(){}
+    @Override
+    public void growth(){}
+    @Override
+    public void fly(){}
+    @Override
+    public void gust(){}
+}
