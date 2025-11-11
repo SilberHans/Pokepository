8da9f783687affe0d0262ca3c7d1e9c620640ad1
adria <adria@10.14.89.29>
2025-11-11 11:47:34 -0500

    Hola

---
+++ src/main/java/Utility/PokeWriting.java
@@ -0,0 +1,20 @@
+
+package Utility;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class PokeWriting {
+    
+    public void Writing(){
+        try{
+            Path path = Files.writeString(Path.of("C:/Pokelist.txt"), "Hola", StandardOpenOption.CREATE_NEW);
+            System.out.println(path);
+            String inf = Files.readString(path);
+            System.out.println(inf);
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        } 
+    }
+}
