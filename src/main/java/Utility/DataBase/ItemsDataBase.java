37ce2311e54a60a724c1299ea4a20cac2cc91973
Owner <Owner@10.14.20.48>
2025-11-17 21:55:28 -0500

    Full Logic 1.2

--- src/main/java/Utility/DataBase/ItemsDataBase.java
+++ src/main/java/Utility/DataBase/ItemsDataBase.java
@@ -12,25 +12,25 @@ public final class ItemsDataBase {
     
     static {
         // --- HP Healing Items ---
-        pkItemsList.add(new Item("Potion", 300, PkEffectsEnum.HealFixedAmount, 20));
-        pkItemsList.add(new Item("Super Potion", 700, PkEffectsEnum.HealFixedAmount, 60));
-        pkItemsList.add(new Item("Hyper Potion", 1200, PkEffectsEnum.HealFixedAmount, 120));
-        pkItemsList.add(new Item("Max Potion", 2500, PkEffectsEnum.HealPercentage, 100));
+        pkItemsList.add(new Item("Potion", "Restores 20 HP.", 300, PkEffectsEnum.HealFixedAmount, 20));
+        pkItemsList.add(new Item("Super Potion", "Restores 60 HP.", 700, PkEffectsEnum.HealFixedAmount, 60));
+        pkItemsList.add(new Item("Hyper Potion", "Restores 120 HP.", 1200, PkEffectsEnum.HealFixedAmount, 120));
+        pkItemsList.add(new Item("Max Potion", "Fully restores HP.", 2500, PkEffectsEnum.HealPercentage, 100));
         
         // --- Status (PkStatus) Healing Items ---
-        pkItemsList.add(new Item("Antidote", 100, PkEffectsEnum.CurePoison, 0));
-        pkItemsList.add(new Item("Paralyze Heal", 200, PkEffectsEnum.CureParalysis, 0));
-        pkItemsList.add(new Item("Awakening", 250, PkEffectsEnum.CureSleep, 0));
-        pkItemsList.add(new Item("Burn Heal", 250, PkEffectsEnum.CureBurn, 0));
-        pkItemsList.add(new Item("Ice Heal", 250, PkEffectsEnum.CureFreeze, 0));
-        pkItemsList.add(new Item("Full Heal", 600, PkEffectsEnum.CureAllStatus, 0));
+        pkItemsList.add(new Item("Antidote", "Cures Poison.", 100, PkEffectsEnum.CurePoison, 0));
+        pkItemsList.add(new Item("Paralyze Heal", "Cures Paralysis.", 200, PkEffectsEnum.CureParalysis, 0));
+        pkItemsList.add(new Item("Awakening", "Cures Sleep.", 250, PkEffectsEnum.CureSleep, 0));
+        pkItemsList.add(new Item("Burn Heal", "Cures Burn.", 250, PkEffectsEnum.CureBurn, 0));
+        pkItemsList.add(new Item("Ice Heal", "Cures Freeze.", 250, PkEffectsEnum.CureFreeze, 0));
+        pkItemsList.add(new Item("Full Heal", "Cures all status conditions.", 600, PkEffectsEnum.CureAllStatus, 0));
 
         // --- Battle (Buff) Items ---
-        pkItemsList.add(new Item("X Attack", 500, PkEffectsEnum.AttackUp2, 0));
-        pkItemsList.add(new Item("X Defense", 550, PkEffectsEnum.DefenseUp2, 0));
-        pkItemsList.add(new Item("X Speed", 350, PkEffectsEnum.SpeedUp2, 0));
-        pkItemsList.add(new Item("X Sp. Atk", 350, PkEffectsEnum.SpecialAttackUp2, 0));
-        pkItemsList.add(new Item("X Sp. Def", 350, PkEffectsEnum.SpecialDefenseUp2, 0));
-        pkItemsList.add(new Item("X Accuracy", 950, PkEffectsEnum.AccuracyUp1, 0));
+        pkItemsList.add(new Item("X Attack", "Raises Attack in battle.", 500, PkEffectsEnum.AttackUp2, 0));
+        pkItemsList.add(new Item("X Defense", "Raises Defense in battle.", 550, PkEffectsEnum.DefenseUp2, 0));
+        pkItemsList.add(new Item("X Speed", "Raises Speed in battle.", 350, PkEffectsEnum.SpeedUp2, 0));
+        pkItemsList.add(new Item("X Sp. Atk", "Raises Special Attack in battle.", 350, PkEffectsEnum.SpecialAttackUp2, 0));
+        pkItemsList.add(new Item("X Sp. Def", "Raises Special Defense in battle.", 350, PkEffectsEnum.SpecialDefenseUp2, 0));
+        pkItemsList.add(new Item("X Accuracy", "Raises Accuracy in battle.", 950, PkEffectsEnum.AccuracyUp1, 0));
     }
 }
